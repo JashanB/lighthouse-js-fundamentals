ddf59383fa83d2fab4438750ae409107222b1cad
Jashan <jashan_brar50@hotmail.com>
2019-10-22 23:53:10 +0000

    3 loops

---
+++ ingredients.js
@@ -0,0 +1,12 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++
+}
+for (let j = 0; j < ingredients.length; j++) {
+  console.log(ingredients[j])
+}
+for (let z = ingredients.length; z = 0; z--) {
+  console.log(ingredients[z])
+}
